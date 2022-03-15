deda99ec6c1da80db0ed18c01ef72a14c26c385b
robsonlm <robsonlmaraia@gmail.com>
2022-03-15 17:29:22 +0000

    Add validation to inventory post

--- routes/inventoryRouter.js
+++ routes/inventoryRouter.js
@@ -1,31 +1,54 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const { uuid } = require("uuidv4");
+const { v4: uuidv4 } = require("uuid");
 
 router
   .get("/", (req, res) => {
     //write your code here
+    res.status(200).json("newVideo");
   })
   .get("/:id", (req, res) => {
     //write your code here
   })
   .post("/", (req, res) => {
-    console.log(req.body);
-    const newItem = {
-      id: uuid(),
-      warehouseID: req.body.warehouseID,
-      warehouseName: req.body.warehouseName,
-      itemName: req.body.itemName,
-      description: req.body.description,
-      category: "Accessories",
-      status: "Out of Stock",
-      quantity: "",
-    };
-    const fileContent = readFile("./data/video-details.json");
-    fileContent.push(newVideo);
-    fs.writeFileSync("./data/video-details.json", JSON.stringify(fileContent));
-    res.status(201).json(newVideo);
+    if (
+      !!req.body.warehouseID &&
+      !!req.body.warehouseName &&
+      !!req.body.itemName &&
+      !!req.body.description &&
+      !!req.body.category &&
+      !!req.body.status &&
+      (req.body.quantity === 0 || !!req.body.quantity)
+    ) {
+      const newItem = {
+        id: uuidv4(),
+        warehouseID: req.body.warehouseID,
+        warehouseName: req.body.warehouseName,
+        itemName: req.body.itemName,
+        description: req.body.description,
+        category: req.body.category,
+        status: req.body.status,
+        quantity: req.body.quantity,
+      };
+      res.status(201).json(newItem);
+    } else {
+      const template = {
+        warehouseID: "req.body.warehouseID",
+        warehouseName: "req.body.warehouseName",
+        itemName: "req.body.itemName",
+        description: "req.body.description",
+        category: "req.body.category",
+        status: "req.body.status",
+        quantity: "req.body.quantity",
+      };
+
+      res
+        .status(400)
+        .json(
+          "The request is missing a value, you have to provide: warehouseID, warehouseName, itemName, description, category, status and quantity"
+        );
+    }
   })
   .put("/:id", (req, res) => {
     //write your code here
